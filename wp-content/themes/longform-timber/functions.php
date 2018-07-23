e6ef96ab18991e5ee348b2c74dd87d1a35215e93
Blaine Levy <blainelevy@Blaines-MBP.cfl.rr.com>
2018-07-23 06:55:22 -0400

    fix functions

--- wp-content/themes/longform-timber/functions.php
+++ wp-content/themes/longform-timber/functions.php
@@ -65,7 +65,7 @@ class StarterSite extends TimberSite {
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_scripts' );
 function my_theme_enqueue_scripts() {
 	wp_enqueue_script( 'bundle', get_stylesheet_directory_uri() . '/dist/js/all.min.js', array('jquery'), 1, false );
-
+}
 
 
 // $timber = new Timber\Timber();
